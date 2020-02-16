3f21f996f615ecec7baa6586ef0c97f86942306a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-17 01:51:57 +1100

    remove pre save and compare function

--- server/api/models/userModel.js
+++ server/api/models/userModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   name: {
@@ -19,28 +17,4 @@ const UserSchema = new Schema({
   }
 }, { collection: 'user' });
 
-UserSchema.pre('save', function(next) {
-  const user = this;
-
-  if (!user.isModified('password')) return next();
-
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-UserSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
-
-module.exports = mongoose.model('User', UserSchema);
+module.exports = User = mongoose.model('User', UserSchema);
