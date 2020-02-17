eb69e97cbf42ea3ac7e82d13772a909e1227653b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-18 00:58:16 +1100

    setting up redux

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Register from './Register';
 
+import { Provider } from 'react-redux';
+import store from '../store';
+import { loadUser } from '../actions/authActions';
+
 function App() {
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Register />
+      </div>
+    </Provider>
   );
 }
 
