9f95d4d8cb10cf5b18e41b658d5711e6aac2ccf4
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-18 00:57:42 +1100

    changing error message

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -8,16 +8,16 @@ exports.loginUser = (req, res) => {
   const { email, password } = req.body;
 
   User.findOne({ email }, (err, user) => {
-    if (err) return res.state(500).send('Error on the server.');
-    if (!user) return res.status(404).send('User not found.');
+    if (err) return res.state(500).send({ error: 'Server error.' });
+    if (!user) return res.status(404).send({ error: 'User does not exist.' });
 
     bcrypt.compare(password, user.password)
       .then(isMatch => {
-        if (!isMatch) return res.status(401).send({ auth: false, token: null })
+        if (!isMatch) return res.status(400).send({ auth: false, token: null })
 
         jwt.sign(
           { id: user.id },
-          config.get('jwtScret'),
+          config.get('jwtSecret'),
           { expiresIn: 3600 },
           (err, token) => {
             if (err) throw err;
