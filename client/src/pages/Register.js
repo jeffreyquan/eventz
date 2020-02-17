6fa3edb244109cb5d9480a75e6332c02c3b25636
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-17 16:13:18 +1100

    styling and validation

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,58 +1,86 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useForm } from "../utils/useForm";
 import axios from 'axios';
+import  { Grid, Button, Form } from 'semantic-ui-react';
+import validateForm from '../utils/validateForm';
 
 const SignUp = () => {
   const [values, handleChange] = useForm({
     name: "",
     email: "",
     password: "",
+    confirmationPassword: "",
   });
+
+  const [disabledButton, setDisabledButton] = useState(true)
+
+  useEffect(() => {
+    setDisabledButton(true);
+  }, []);
+
+  useEffect(() => {
+    const errors = validateForm(values);
+    console.log(errors);
+  }, [values]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .post("http://localhost:5000/api/users/register", values)
-      .then(res => {
-        console.log('AAAAA');
-      })
-      .catch(err => {
-        console.log(err.response.data);
-      });
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="name"
-          placeholder="Name"
-          value={values.name}
-          onChange={handleChange}
-          required
-        />
-        <input
-          name="email"
-          placeholder="Email"
-          value={values.email}
-          onChange={handleChange}
-          required
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={values.password}
-          onChange={handleChange}
-          required
-        />
-        <button
-          type="submit"
-        >
-          Sign Up
-        </button>
-      </form>
-    </div>
+    <Grid columns={1} container centered style={{ height: "100vh" }}>
+      <Grid.Row>
+        <Grid.Column width={4} verticalAlign="middle">
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <label style={{ textAlign: "left" }}>Name</label>
+              <Form.Input
+                name="name"
+                placeholder="Name"
+                value={values.name}
+                onChange={handleChange}
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label style={{ textAlign: "left" }}>Email</label>
+              <Form.Input
+                name="email"
+                placeholder="Email"
+                value={values.email}
+                onChange={handleChange}
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label style={{ textAlign: "left" }}>Password</label>
+              <Form.Input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={values.password}
+                onChange={handleChange}
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label style={{ textAlign: "left" }}>Confirm Password</label>
+              <Form.Input
+                type="password"
+                name="confirmationPassword"
+                placeholder="Confirm Password"
+                value={values.confirmationPassword}
+                onChange={handleChange}
+                required
+              />
+            </Form.Field>
+            <Button type="submit" disabled={disabledButton}>
+              Sign Up
+            </Button>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 };
 
