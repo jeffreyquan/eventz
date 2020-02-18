8c96f1a3f9ce817d0527520e943d3c53415961ff
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-18 21:42:28 +1100

    routes to login user and load user

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -1,7 +1,9 @@
 const authController = require('../controllers/authController');
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-router.post("/", authController.loginUser);
+router.post('/', authController.loginUser);
+router.get('/user', auth, authController.loadUser);
 
 module.exports = router;
