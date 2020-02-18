80263da0af475ee36678c9fa6ed947d225a1356d
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-18 21:40:21 +1100

    added login function

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -15,12 +15,14 @@ export const loadUser = () => (dispatch, getState) => {
 
   dispatch({ type: USER_LOADING });
 
-  axios.get('http://localhost:5000/api/auth/', tokenConfig(getState))
+  axios
+    .get('http://localhost:5000/api/auth/user', tokenConfig(getState))
     .then(res => dispatch({
       type: USER_LOADED,
       payload: res.data
     }))
     .catch(err => {
+      console.log(err.response);
       dispatch(returnErrors(err.response.data, err.response.status));
       dispatch({
         type: AUTH_ERROR
@@ -38,7 +40,8 @@ export const register = ({ name, email, password }) => dispatch => {
 
   const body = JSON.stringify({ name, email, password });
 
-  axios.post('http://localhost:5000/api/users/register', body, config)
+  axios
+    .post('http://localhost:5000/api/users/register', body, config)
     .then(res => dispatch({
       type: REGISTER_SUCCESS,
       payload: res.data
@@ -49,6 +52,32 @@ export const register = ({ name, email, password }) => dispatch => {
       );
       dispatch({
         type: REGISTER_FAIL
+      })
+    })
+}
+
+export const login = ({ email, password }) => dispatch => {
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const body = JSON.stringify({ email, password });
+
+  axios
+    .post('http://localhost:5000/api/auth', body, config)
+    .then(res => dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    }))
+    .catch(err => {
+      dispatch(
+        returnErrors(err.response.data, err.response.status, 'LOGIN_FAIL')
+      );
+      dispatch({
+        type: LOGIN_FAIL
       })
     })
 }
