c36b0bc05cad1ba54cf228b4cf1d7cdb026a3688
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-19 17:34:20 +1100

    created Home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Grid, Menu } from "semantic-ui-react";
 
 const Home = () => {
   return (
-    <div>
-      Home Page coming soon
-    </div>
-  )
+    <Grid container centered style={{ height: "100vh" }}>
+      <Grid.Row>
+        <Grid.Column width={8} verticalAlign="middle">
+          Home Page coming soon
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
 
 export default Home;
