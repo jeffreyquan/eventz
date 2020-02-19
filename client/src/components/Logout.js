5695504f7b86b3eba7c968d424fd18984cab115c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-20 01:08:37 +1100

    added button and destructured prop

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from 'react';
 import { logout } from '../actions/authActions';
 import { connect } from 'react-redux';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 
-const Logout = props => {
+const Logout = ({ logout }) => {
 
   return (
     <Fragment>
       <Menu.Item
         name="logout"
-        onClick={props.logout}
+        onClick={ logout }
       >
-        Logout
+        <Button>Logout</Button>
       </Menu.Item>
     </Fragment>
   )
