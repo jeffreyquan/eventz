a4d634998dbd1686bd82aef6f2c7c011593707fa
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-19 17:34:01 +1100

    updated import App

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "semantic-ui-css/semantic.min.css";
-import App from './pages/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
