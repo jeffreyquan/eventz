853c217cc5e4a67052a9254d22a983d27265971f
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-02-19 17:32:08 +1100

    created navbar component

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+  const [activeItem, setActiveItem] = useState(null);
+
+  const handleClick = (e) => setActiveItem(e.target.value);
+
+  return (
+    <Menu fixed='top'>
+      <Menu.Item
+        as={Link}
+        to="/"
+        name="home"
+        active={activeItem === "home"}
+        onClick={handleClick}
+      >
+        Home
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item
+          as={Link}
+          to="/register"
+          name="register"
+          active={activeItem === "register"}
+          onClick={handleClick}
+        >
+          Register
+        </Menu.Item>
+        <Menu.Item
+          as={Link}
+          to="/login"
+          name="login"
+          active={activeItem === "login"}
+          onClick={handleClick}
+        >
+          Login
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+}
+
+export default Navbar;
